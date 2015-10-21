cebd7bbf282afa820aea1678dde48bbc860cad97
GlowKitty <iamtheglowkitty@gmail.com>
2015-10-21 10:51:24 -0400

    update MeowBotC and LapFoxBot

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo ">>>Compiling MeowBot"
-javac -cp .:lib/pircbot.jar:lib/commons-digester-2.1.jar:lib/commons-digester-rss.jar -d . @.sources -Xlint:unchecked
+javac -cp .:lib/pircbot.jar:lib/commons-logging-2.1.jar:lib/commons-digester-2.1.jar:lib/commons-digester-rss.jar -d . @.sources -Xlint:unchecked
 echo ">>>End"
