2f0778f914846f68714c6a5b2fa4ec45f2a12445
GlowKitty <iamtheglowkitty@gmail.com>
2015-10-23 13:43:05 -0400

    v2.0 ready

--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo ">>>Compiling MeowBot"
 javac -cp .:lib/pircbot.jar:lib/commons-logging-1.2.jar:lib/commons-beanutils-1.9.2.jar:lib/commons-digester-2.1.jar:lib/commons-digester-rss.jar -d . @.sources -Xlint:unchecked
+echo ">>>Creating .jar file"
+jar cfm MeowBot.jar manifest org/meowbot/*.class
 echo ">>>End"
