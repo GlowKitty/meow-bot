8a3c2525227cb3454591c3dec467c687ab0cc1a1
GlowKitty <iamtheglowkitty@gmail.com>
2015-11-04 17:18:43 -0500

    run.sh now creates a jar file when run

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo ">>>Compiling MeowBot"
 javac -cp .:lib/pircbot.jar:lib/commons-logging-1.2.jar:lib/commons-beanutils-1.9.2.jar:lib/commons-digester-2.1.jar:lib/commons-digester-rss.jar -d . @.sources -Xlint:unchecked
+echo ">>>Compiling completed"
 echo ">>>Creating .jar file"
 jar cfm MeowBot.jar manifest org/meowbot/*.class
+echo ">>>Jar file created"
 echo ">>>End"
--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 echo ">>>Compiling MeowBot"
 javac -cp .:lib/pircbot.jar:lib/commons-logging-1.2.jar:lib/commons-beanutils-1.9.2.jar:lib/commons-digester-2.1.jar:lib/commons-digester-rss.jar -d . @.sources -Xlint:unchecked
 echo ">>>Compiling completed"
+echo ">>>Creating jar file"
+jar cfm MeowBot.jar manifest org/meowbot/*.class
+echo ">>>Jar file created"
 echo ">>>Running MeowBot"
 java -cp .:lib/pircbot.jar:lib/commons-logging-1.2.jar:lib/commons-beanutils-1.9.2.jar:lib/commons-digester-2.1.jar:lib/commons-digester-rss.jar org.meowbot.MeowBotMain
 echo ">>>End"
